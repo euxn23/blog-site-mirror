9fe75377b8f0c918ff48437d48ecf17230bd7453
euxn23 <euxn23@gmail.com>
2023-05-21 23:21:37 +0900

    feat: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Oswald } from 'next/font/google'
 import './layout.scss'
 import Link from 'next/link'
 import { injectPageToMetadata } from '@/src/helper/inject-page-to-metadata'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitterSquare, faGithubSquare } from '@fortawesome/free-brands-svg-icons'
 
 const oswald = Oswald({ subsets: ['latin'] })
 
@@ -19,13 +21,31 @@ export default function RootLayout({
         <div className="layout">
           <div className="container">
             <Link className="header" href="/">
-              <img
-                className="icon"
-                src="/icon.png"
-              />
+              <img className="icon" src="/icon.png" />
               <div className="title">blog.euxn.me</div>
             </Link>
             {children}
+            <div className="footer">
+              <div className="links">
+                <a
+                  href="https://twitter.com/euxn23"
+                  target="_blank"
+                  className="twitter"
+                >
+                  <FontAwesomeIcon icon={faTwitterSquare} />
+                </a>
+                <a
+                  href="https://github.com/euxn23"
+                  target="_blank"
+                  className="github"
+                >
+                  <FontAwesomeIcon icon={faGithubSquare} />
+                </a>
+              </div>
+              <div className="copylight">
+                <div className="euxn">©2023 SUZUKI Yuta.&nbsp;<span className="nao">designed by ENDO Nao.</span></div>
+              </div>
+            </div>
           </div>
         </div>
       </body>
