588fff9cfef6ad078f2b62fea89970cbc8b7633d
euxn23 <euxn23@gmail.com>
2023-05-26 14:55:36 +0900

    fix: fix regexp

--- src/app/entry/[slug]/page.tsx
+++ src/app/entry/[slug]/page.tsx
@@ -17,7 +17,7 @@ type Props = {
 
 const components: Components = {
   code({ node, inline, className, children, ...props }) {
-    const match = /language-(\w+)(:.+)/.exec(className || '')
+    const match = /language-(\w+)(:.+)?/.exec(className || '')
     return !inline && match ? (
       // @ts-expect-error Server Component
       <Code {...props} theme={'one-dark-pro'} lang={match[1]} title={match[2]?.slice(1)} lineNumbers>
