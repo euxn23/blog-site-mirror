e9a8dc78c6e013950cf16382f4d0142b7637f351
euxn23 <euxn23@gmail.com>
2023-05-26 13:56:42 +0900

    feat: add filename to codeblock

--- src/app/entry/[slug]/page.tsx
+++ src/app/entry/[slug]/page.tsx
@@ -17,10 +17,10 @@ type Props = {
 
 const components: Components = {
   code({ node, inline, className, children, ...props }) {
-    const match = /language-(\w+)/.exec(className || '')
+    const match = /language-(\w+)(:.+)/.exec(className || '')
     return !inline && match ? (
       // @ts-expect-error Server Component
-      <Code {...props} theme={'one-dark-pro'} lang={match[1]} lineNumbers>
+      <Code {...props} theme={'one-dark-pro'} lang={match[1]} title={match[2]?.slice(1)} lineNumbers>
         {String(children).replace(/\n$/, '')}
       </Code>
     ) : (
